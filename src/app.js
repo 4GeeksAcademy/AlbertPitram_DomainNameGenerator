b8f03f2b5af71194acb98b127d5acb5109231ba1
AlbertPitram <albertvidallopez02@gmail.com>
2025-05-14 19:28:03 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //Arrays with the content
+  const pronoun = ['the', 'our', 'us', 'my'];
+  const adj = ['skibidi', 'mewing', 'rizzler', 'based'];
+  const noun = ['Balatro', 'Ibai', 'Solaire', 'JohnFallout'];
+  const extension =['.net', '.cat', '.com', '.io']
+  
+  //For loop
+  for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let  n= 0; n< noun.length;n++) {
+      for (let e = 0; e < extension.length; e++) {
+        const domain = pronoun[p] + adj[a] + noun[n] + extension[e];
+        console.log(domain);
+      }
+    }
+  }
+}
+}
+
