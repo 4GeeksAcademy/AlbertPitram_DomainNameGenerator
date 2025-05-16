eb680e97a45c8f5cbed248ecef83289d97313c45
AlbertPitram <albertvidallopez02@gmail.com>
2025-05-16 19:30:01 +0000

    Add new for function

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ window.onload = function() {
   //For loop
   for (let p = 0; p < pronoun.length; p++) {
   for (let a = 0; a < adj.length; a++) {
-    for (let  n= 0; n< noun.length;n++) {
+    for (let  n= 0; n < noun.length; n++) {
       for (let e = 0; e < extension.length; e++) {
         const domain = pronoun[p] + adj[a] + noun[n] + extension[e];
         console.log(domain);
@@ -25,4 +25,21 @@ window.onload = function() {
   }
 }
 }
+//For of option
+function generateDomain (pronoun, adj, noun, extension){
+  const domains =[];
+  
+  for (const p of pronoun) {
+    for (const a of adj) {
+      for (const n of noun){
+        for (const e of extension){
+          domains.push(`${p}${a}${n}${e}`);
+        }
+      }
+    }
+  }
+    return domains;
+}
 
+const domains = generateDomain(pronoun, adj, noun, extension);
+domains.forEach(domains => console.log(domains));
